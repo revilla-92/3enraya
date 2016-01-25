740c32b79bd59e5517ca1c6c30a314e1f6a12165
revilla-92 <daniel.revilla.twose@gmail.com>
2016-01-25 16:58:05 +0100

    Added style and improved check winner function

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,16 +1,18 @@
 const Tablero = require('./Tablero.jsx');
 const Cabecera = require('./Cabecera.jsx');
-const JUGADORX = "jugador 1 - las X";
-const JUGADOR0 = "jugador 2 - los 0";
+const JUGADORX = "Jugador 1 - las X";
+const JUGADOR0 = "Jugador 2 - los 0";
 
+// Funcion para comprobar que jugador ha ganado.
 function compruebaGanador(valores){
 
-	var nab = 9;
+	// Numero total de movimientos.
+	var n = 9;
 
 	// Comprobaciones para filas y columnas.
 	for (var a = 0; a < 3; a++){
 		
-		// Contadores para coincidencias en fila de 3.
+		// Contadores para coincidencias.
 		var n1X = 0;
 		var n2X = 0;
 		var n3X = 0;
@@ -19,126 +21,78 @@ function compruebaGanador(valores){
 		var n20 = 0;
 		var n30 = 0;
 		var n40 = 0;
-		var juegoAcabado = false;
+
+		// Variable para indicar que el juego ha acabado con ganador.
+		var juegoConGanador = false;
 
 		// Bucle secundario para comprobar las filas y columnas.
 		for (var b = 0; b < 3; b++){
 
-			// Comprobamos que ya no hay más casillas en el tablero por rellenar.
-			if(valores[a][b] === 'X' || valores[a][b] === '0'){
-				nab--;
-			}
-
-			// Comprobamos filas para el valor X
+			// Comprobamos filas para el valor X.
 			if(valores[a][b] === 'X'){
 				n1X++;
+				n--;
 			}
-			// Comprobamos columnas para el valor X
+			// Comprobamos columnas para el valor X.
 			if(valores[b][a] === 'X'){
 				n2X++;
-			}
-			// Comprobamos filas para el valor 0
-			if(valores[a][b] === '0'){
-				n10++;
-			}
-			// Comprobamos columnas para el valor 0
-			if(valores[b][a] === '0'){
-				n20++;
+				n--;
 			}
 
-			// Comprobamos la diagonal principal.
+			// Comprobamos la diagonal principal para el valor X.
 			if (valores[b][b] === 'X'){
 				n3X++;
+				n--;
 			}
 
-			if (valores[b][b] === '0'){
-				n30++;
-			}
-
-			// Comprobamos la diagonal inversa.
+			// Comprobamos la diagonal inversa para el valor X.
 			if (valores[b][2-b] === 'X'){
 				n4X++;
+				n--;
 			}
 
-			if(valores[b][2-b] === '0'){
+			// Comprobamos filas para el valor 0.
+			if(valores[a][b] === '0'){
+				n10++;
+				n--;
+			}
+			// Comprobamos columnas para el valor 0.
+			if(valores[b][a] === '0'){
+				n20++;
+				n--;
+			}
+
+			// Comprobamos la diagonal principal para el valor 0.
+			if (valores[b][b] === '0'){
+				n30++;
+				n--;
+			}
+
+			// Comprobamos la diagonal inversa para el valor 0.
+			if (valores[b][2-b] === '0'){
 				n40++;
-			}
-
-			// Si se termina el juego y nadie ha ganado se invita a jugar otra partida.
-			if(nab === 0){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Juego Terminado. Nadie ha ganado. \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			// Si alguno de los contadores ha llegado a 3 coincidencias con X es que ha hecho 3 en raya y finaliza el juego.
-			if(n1X === 3 || n2X === 3){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Ha ganado el jugador X. \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			// Si alguno de los contadores ha llegado a 3 coincidencias con X es que ha hecho 3 en raya y finaliza el juego.
-			if(n10 === 3 || n20 === 3){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Ha ganado el jugador 0 \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			if(n3X === 3){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Ha ganado el jugador X \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			if(n30 === 3){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Ha ganado el jugador 0 \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			if(n4X === 3){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Ha ganado el jugador X \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			if(n40 === 3){
-				juegoAcabado = true;
-				ponerACero(n1X, n2X, n3X, n4X, n10, n20, n30, n40);
-				var x = confirm("Ha ganado el jugador 0 \n ¿Desea empezar otra partida?");
-				if(x === true){
-					return true;
-				}
-			}
-
-			if(juegoAcabado === true){
-				break;
+				n--;
 			}
 		}
 	}
-};
 
-function ponerACero(a, b, c, d, e, f, g, h){
-	a = b = c = d = e = f = g = h = 0;
+	// Si alguno de los contadores ha llegado a 3 coincidencias con X es que ha hecho 3 en raya y finaliza el juego.
+	if(n1X === 3 || n2X === 3 || n3X === 3 || n4X ===3){
+		juegoConGanador = true;
+		return confirm("Ha ganado el jugador X. \n ¿Desea empezar otra partida?");
+	}
+
+	// Si alguno de los contadores ha llegado a 3 coincidencias con X es que ha hecho 3 en raya y finaliza el juego.
+	if(n10 === 3 || n20 === 3 || n30 === 3 || n40 ===3){
+		juegoConGanador = true;
+		return confirm("Ha ganado el jugador 0 \n ¿Desea empezar otra partida?");
+	}
+
+	// Si se ha acabado el juego sin ganador.
+	if(n === 0 && !juegoConGanador){
+		return confirm("Juego Terminado. Nadie ha ganado. \n ¿Desea empezar otra partida?");
+	}
+
 };
 
 var App = React.createClass({
@@ -189,7 +143,7 @@ var App = React.createClass({
 			<div>
 				<Cabecera texto={texto}/>
 				<Tablero valores={this.state.valores} manejadorTableroClick={this.appClick}/>
-				<button onClick={this.getInitialStateButton} > Reiniciar Partida. </button>
+				<button id="reset" onClick={this.getInitialStateButton} > Reiniciar Partida. </button>
 			</div>
 		)
 	}
